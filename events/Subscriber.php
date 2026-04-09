b9676f612e377d66acc22947deffa8484f78f7df
jkljan <juanandres19.j@gmail.com>
2026-04-08 22:07:25 -0500

    edit

--- events/Subscriber.php
+++ events/Subscriber.php
@@ -1,4 +1,7 @@
 <?php
+// Interfaz que define el contrato para los suscriptores (patrón Observer)
 interface Subscriber {
+    // Método que se ejecuta cuando ocurre un evento
+    // $data contiene la información relevante del evento
     public function update($data);
 }
